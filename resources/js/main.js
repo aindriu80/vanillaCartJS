b4890bfc6c9e18b31695951c95c1f380d724a2e0
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-09-13 20:45:21 +0100

    identifying specific items of DOM

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,6 +4,15 @@ const addToCartButton = document.querySelectorAll(
   '[data-action="ADD_TO_CART"]'
 );
 console.log(addToCartButton);
-addToCartButton.forEach(function(addToCartButtonDOM) {
-  console.log(addToCartButtonDOM);
+addToCartButton.forEach(addToCartButtonDOM => {
+  addToCartButtonDOM.addEventListener("click", () => {
+    console.log(addToCartButtonDOM.parentNode);
+    const productDOM = addToCartButtonDOM.parentNode;
+    const product = {
+      image: productDOM.querySelector(".product__image").getAttribute("src"),
+      name: productDOM.querySelector(".product__name").innerText,
+      price: productDOM.querySelector(".product__price").innerText
+    };
+    console.table(product);
+  });
 });
