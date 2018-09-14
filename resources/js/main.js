823d7938b0bbcedfdee33fc67fd46e22c5488a26
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-09-14 22:39:10 +0100

    adding products to the cart

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,18 +1,42 @@
 "use strict";
 
-const addToCartButton = document.querySelectorAll(
+let cart = [];
+const cartDOM = document.querySelector(".cart");
+const addToCartButtonDOM = document.querySelectorAll(
   '[data-action="ADD_TO_CART"]'
 );
-console.log(addToCartButton);
-addToCartButton.forEach(addToCartButtonDOM => {
+
+// console.log(addToCartButton);
+addToCartButtonDOM.forEach(addToCartButtonDOM => {
   addToCartButtonDOM.addEventListener("click", () => {
-    console.log(addToCartButtonDOM.parentNode);
+    // console.log(addToCartButtonDOM.parentNode);
     const productDOM = addToCartButtonDOM.parentNode;
     const product = {
       image: productDOM.querySelector(".product__image").getAttribute("src"),
       name: productDOM.querySelector(".product__name").innerText,
       price: productDOM.querySelector(".product__price").innerText
     };
-    console.table(product);
+
+    const isInCart =
+      cart.filter(cartItem => cartItem.name === product.name).length > 0;
+
+    if (!isInCart) {
+      cartDOM.insertAdjacentHTML(
+        "beforeend",
+        `
+      <div class="cart__item>
+      <img class="cart__item__image" src="${product.image}" alt="${
+          product.name
+        }">
+      <h3 class="class__item__name">${product.name}</h3>
+      <h3 class="class__item__price">${product.price}</h3>
+      </div>
+      `
+      );
+      cart.push(product);
+      addToCartButtonDOM.innerText = "In Cart";
+
+      // console.log(cart);
+    }
   });
 });
